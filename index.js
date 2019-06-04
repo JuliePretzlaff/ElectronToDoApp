d5d78a02644d6f01dc43bcc128d646c551d5b0ea
Julia <Juliapretzlaff@gmail.com>
2019-06-03 17:21:12 -0700

    added a second window

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New To Do'
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+}
 
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
       {
-        label: 'New To Do'
+        label: 'New To Do',
+        click() {
+          createAddWindow();
+        }
       },
       {
         label: 'Quit',
-        // accelerator: 'Command+Q',
-        // accelerator: (() => {
-        //   if (process.platform === 'darwin') {
-        //     return 'Command+Q';
-        //   } else {
-        //     return 'Ctrl+Q';
-        //   }
-        // })(),
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
         click() {
           app.quit();
