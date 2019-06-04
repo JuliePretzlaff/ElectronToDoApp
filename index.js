18b460c338645f4b60da7a4b4c36f44dc55fa8a3
Julia <Juliapretzlaff@gmail.com>
2019-06-03 18:06:07 -0700

    sending input value from add window to electron to main window

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const electron = require('electron');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, ipcMain, Menu } = electron;
 
 let mainWindow;
 let addWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
   mainWindow.on('closed', () => app.quit());
 
@@ -18,10 +22,21 @@ function createAddWindow() {
   addWindow = new BrowserWindow({
     width: 300,
     height: 200,
-    title: 'Add New To Do'
+    title: 'Add New To Do',
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
   addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on('closed', () => {
+    addWindow = null;
+  });
 }
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo);
+  addWindow.close();
+});
 
 const menuTemplate = [
   {
@@ -46,4 +61,21 @@ const menuTemplate = [
 
 if (process.platform === 'darwin') {
   menuTemplate.unshift({ label: '' });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'Debug',
+    submenu: [
+      { role: 'reload' },
+      {
+        label: 'toggle dev tools',
+        accelerator:
+          process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
 }
