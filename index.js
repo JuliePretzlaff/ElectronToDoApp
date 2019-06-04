42bb8fbb7eaaa57f2f27e9597fc48942b5068248
Julia <Juliapretzlaff@gmail.com>
2019-06-03 17:05:00 -0700

    added basic menu

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+  { label: '' },
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New To Do'
+      }
+    ]
+  }
+];
